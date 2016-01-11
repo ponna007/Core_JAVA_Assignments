a8095c6a2c85da2b6583038d87ed0d35315fb3f5
ponna007 <ponna007@gmail.com>
2016-01-11 16:28:14 +0530

    Session 1 : Assignment 3

---
+++ Session_1/notInt.java
@@ -0,0 +1,14 @@
+
+public class notInt {
+
+	public static void main(String[] args) {
+		int a=10000000000;
+		int b=14.9;
+		int c="Preetham";
+		System.out.println("The value of a is "+a);
+		System.out.println("The value of b is "+b);
+		System.out.println("The value of c is "+c);
+
+	}
+
+}
