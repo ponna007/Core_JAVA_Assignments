65c2a11fa8d830c6721b20cca89ec505e589e5aa
ponna007 <ponna007@gmail.com>
2016-01-11 15:49:56 +0530

    Session 1 : Assignment 1

---
+++ Session_1/sumOfTwo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sumOfTwo {
+
+	public static void main(String[] args) {
+				System.out.println("Enter a integer type number");
+				Scanner scanner = new Scanner(System.in);
+				int a= scanner.nextInt();
+				System.out.println("Enter another floating type number");
+				float b= scanner.nextFloat();
+				int sum=a+(int)b;
+				System.out.println("The sum is "+sum);
+				
+	}
+}
+		
