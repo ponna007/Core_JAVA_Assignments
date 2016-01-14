b384b20c4dce956bf5f46afe38fe80214e8485d1
ponna007 <ponna007@gmail.com>
2016-01-14 13:13:18 +0530

    Session 1 : Assignment 4

---
+++ Session_1/expressions.java
@@ -0,0 +1,13 @@
+
+public class expressions {
+
+	public static void main(String[] args) {
+		int x=5,y=10;
+		System.out.println("x+y*2="+(x+y*2));
+		System.out.println("x-y+2="+(x-y+2));
+		System.out.println("(x+y)*2="+((x+y)*2));
+		System.out.println("y%x="+(y%x));
+
+	}
+
+}
